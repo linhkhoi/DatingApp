dfbb1de825e398d663bf0cf61987779791c99160
linhkhoi <linhkhoi324@gmail.com>
2021-05-20 18:15:19 +0700

    update code AutoMapper

---
+++ API/Data/Seed.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(DataContext context)
+        {
+            if(await context.User.AnyAsync()) return;
+
+            var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+            var users = JsonConvert.DeserializeObject<List<AppUser>>(userData);
+            foreach (var user in users)
+            {
+                using var hmac = new HMACSHA512();
+
+                user.Username = user.Username.ToLower();
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+                user.PasswordSalt = hmac.Key;
+
+                context.User.Add(user);
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
