c136ec27419a89f634d1acff93969bd5cb3b710a
linhkhoi <linhkhoi324@gmail.com>
2021-05-29 23:04:04 +0700

    Update Like

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class BuggyController: BaseApiController
+    public class BuggyController : BaseApiController
     {
         private readonly DataContext _context;
         public BuggyController(DataContext context)
@@ -20,29 +20,31 @@ namespace API.Controllers
         {
             return "secret text";
         }
-        
+
         [HttpGet("not-found")]
         public ActionResult<AppUser> GetNotFound()
         {
             var thing = _context.User.Find(-1);
+
             if (thing == null) return NotFound();
+
             return Ok(thing);
         }
-        
+
         [HttpGet("server-error")]
         public ActionResult<string> GetServerError()
         {
             var thing = _context.User.Find(-1);
-            var thingToReturn = thing.ToString();
-            return thingToReturn;
 
+            var thingToReturn = thing.ToString();
+
+            return thingToReturn;
         }
-        
+
         [HttpGet("bad-request")]
         public ActionResult<string> GetBadRequest()
         {
-            return BadRequest("This was not a good request");
+            return BadRequest();
         }
-
     }
 }
--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -5,10 +5,10 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
-import {NavigationExtras, Router} from "@angular/router";
-import {ToastrService} from "ngx-toastr";
-import {catchError} from "rxjs/operators";
+import { Observable, throwError } from 'rxjs';
+import { Router, NavigationExtras } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -18,29 +18,31 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError(error => {
-        if(error) {
-          switch (error.status){
+        if (error) {
+          switch (error.status) {
             case 400:
-              if (error.error.errors){
+              if (error.error.errors) {
                 const modalStateErrors = [];
-                for (const key in error.error.errors){
-                  if(error.error.errors[key]){
-                    modalStateErrors.push(error.error.errors[key]);
+                for (const key in error.error.errors) {
+                  if (error.error.errors[key]) {
+                    modalStateErrors.push(error.error.errors[key])
                   }
                 }
                 throw modalStateErrors.flat();
-              } else {
+              } else if (typeof(error.error) === 'object') {
                 this.toastr.error(error.statusText, error.status);
+              } else {
+                this.toastr.error(error.error, error.status);
               }
               break;
             case 401:
-              this.toastr.error(error.statusText === 'OK' ? 'Unauthorised' : error.statusText, error.status);
+              this.toastr.error(error.statusText, error.status);
               break;
             case 404:
               this.router.navigateByUrl('/not-found');
               break;
             case 500:
-              const navigationExtras: NavigationExtras = {state: {error: error.error}};
+              const navigationExtras: NavigationExtras = {state: {error: error.error}}
               this.router.navigateByUrl('/server-error', navigationExtras);
               break;
             default:
@@ -51,6 +53,6 @@ export class ErrorInterceptor implements HttpInterceptor {
         }
         return throwError(error);
       })
-    );
+    )
   }
 }
